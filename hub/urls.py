675ab258851cd54d3a9bbd8333f3b32f2372f194
Dayeon Lee <iamdayeonlee@gmail.com>
2021-03-16 17:39:25 +0900

    Add tutorial_detail view, url, template

--- hub/urls.py
+++ hub/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import HomeView
+from .views import HomeView, TutorialDetailsView
 
 app_name = 'hub'
 
 urlpatterns = [
 	path('', HomeView.as_view(), name='home'),
 	path('tutorial_list/', views.TutorialsList, name='tutorial_list'),
+	path('tutorial_details/<int:pk>', TutorialDetailsView.as_view(), name='tutorial_detail'),
 
 ]
--- hub/views.py
+++ hub/views.py
@@ -10,7 +10,14 @@ class HomeView(ListView):
 
 def TutorialsList(request):
 	tutorials = Tutorial.objects.all()
-	# tutorial = tutorials.tutoriallist_set.all() parent child relatioship case
+	# tutorial = tutorials.tutoriallist_set.all() parent child relationship case
 	myFilter = TutorialFilter(request.GET, queryset=tutorials)
 	tutorials = myFilter.qs
 	return render(request, 'tutorial_list.html', {'tutorials': tutorials, 'myFilter':myFilter})
+# tutorial_list, line 85,  <td><a href="{% url 'tutorial_detail' item.pk %}">{{item.title}}</a></td>
+
+
+# <a href="{% url 'tutorial-detail' post.pk %}"></a>
+class TutorialDetailsView(DetailView):
+    model = Tutorial
+    template_name = 'tutorial_detail.html'
