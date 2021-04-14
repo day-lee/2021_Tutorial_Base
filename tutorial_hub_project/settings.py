fd343b87a6c19c390829dc73fe4c1f67ff12db34
Dayeon Lee <iamdayeonlee@gmail.com>
2021-04-14 22:59:28 +0900

    Cancel Comment out DATABASE for sqlite3 for testing

--- tutorial_hub_project/settings.py
+++ tutorial_hub_project/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'tutorial_hub_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
